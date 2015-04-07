e1a462163cd9cdc5d096c9b75a3704f2d0af599e
Alex Foster <fosteralexc@gmail.com>
2015-04-06 21:53:41 -0500

    Formatting register.php and removing redundant test file

--- application/views/register.php
+++ application/views/register.php
@@ -1,32 +1,35 @@
 <html>
 <head>
-<title>My Form</title>
+	<title>My Form</title>
 </head>
 <body>
 
 <?php echo validation_errors(); ?>
 
 <?php echo form_open('register'); ?>
+<form>
 
-<h5>Username</h5>
-<input type="text" name="username" value="<?php echo set_value('username'); ?>" size="50" />
+	<h5>Username</h5>
+		<input type="text" name="username" value="<?php echo set_value('username'); ?>" size="50" />
 
-<h5>Password</h5>
-<input type="password" name="password" value="<?php echo set_value('password'); ?>" size="50" />
+	<h5>Password</h5>
+		<input type="password" name="password" value="<?php echo set_value('password'); ?>" size="50" />
 
-<h5>Password Confirm</h5>
-<input type="password" name="passconf" value="<?php echo set_value('passconf'); ?>" size="50" />
+	<h5>Password Confirm</h5>
+		<input type="password" name="passconf" value="<?php echo set_value('passconf'); ?>" size="50" />
 
-<h5>Email Address</h5>
-<input type="text" name="email" value="<?php echo set_value('email'); ?>" size="50" />
+	<h5>Email Address</h5>
+		<input type="text" name="email" value="<?php echo set_value('email'); ?>" size="50" />
 
-<h5>nation</h5>
-<input type="radio" name="nation" value="native"> native<br>
-<input type="radio" name="nation" value="international"> international<br>
+	<h5>nation</h5>
+		<input type="radio" name="nation" value="native"> native<br>
+		<input type="radio" name="nation" value="international"> international<br>
 
-<div><input type="submit" value="Submit" /></div>
+	<div>
+		<input type="submit" value="Submit" />
+	</div>
 
 </form>
-<img src="<?php echo base_url('images/mizzou.jpg'); ?>" alt="image">
+	<img src="<?php echo base_url('images/mizzou.jpg'); ?>" alt="image">
 </body>
 </html>
