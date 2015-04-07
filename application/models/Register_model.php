acf7d361829e63fb29e19740a10d66c5a14da07f
Liu,Qintai <qltf8@mail.missouri.edu>
2015-04-06 22:19:21 -0500

    updata insert data into databse

--- application/models/Register_model.php
+++ application/models/Register_model.php
@@ -7,8 +7,9 @@
 		public function insert($array){
 			$sql="insert into user values(?,?,?)";
 			$data[0]=$array['username'];
-			$data[1]=$array['password'];
-			$data[2]=$array['nation'];
+			$data[1]=md5($array['password']);
+			$data[2]=$array['eamail']
+			$data[3]=$array['nation'];
 			$bol=$this->db->query($sql,$data);
 			if($bol)
 				echo "successs";
