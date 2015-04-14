29fc5b7b235836240df0fc07f1ef34a971297f14
Ben Rogers <ben@Ben-Rogers-MacBook-Pro.local>
2015-04-13 20:19:08 -0500

    Temp to fix conflicts

--- application/models/Register_model.php
+++ application/models/Register_model.php
@@ -3,7 +3,7 @@
 		public function __construct(){
 			parent::__construct();
 			$this->load->database();
-			//$this->db->query("use test");
+			$this->db->query("use test");
 		}
 		public function insert($array){
 			$sql="insert into user values(?,?,?)";
