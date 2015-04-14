e4ee19201769a6f2b94cca12695240151e711141
Shane Guyette <shaneguyette@mwc-229201.dhcp.missouri.edu>
2015-04-13 20:55:59 -0500

    Added a css stylesheet and possible homepage design

---
+++ application/views/Homepage.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Homepage</title>
+<meta name="keywords" content="" />
+<meta name="description" content="" />
+<link href="http://fonts.googleapis.com/css?family=Arvo:400,700" rel="stylesheet" type="text/css" />
+<link href="hp.css" rel="stylesheet" type="text/css" media="all" />
+</head>
+<body>
+<div id="wrapper">
+	<div id="wrapper-bgtop">
+		<div id="header-wrapper">
+			<div id="header">
+				<div id="logo">
+					<h1><a href="#">Group L</a></h1>
+				</div>
+				<div id="menu">
+					<ul>
+						<li class="active"><a href="#" accesskey="1" title="">Home</a></li>
+						<li><a href="#" accesskey="2" title="">Application</a></li>
+						<li><a href="#" accesskey="4" title="">About</a></li>
+						<li><a href="#" accesskey="5" title="">Sign in</a></li>
+					</ul>
+				</div>
+			</div>
+		</div>
+		<div id="banner" class="container"><img src="images/mizzou.jpg" width="1000" height="500" alt="" /></div>
+		<div id="page" class="container">
+			<div id="content">
+				<div>
+					<h2>Welcome</h2>
+					<p>Pellentesque pede. Donec pulvinar ullamcorper metus. In eu odio at lectus pulvinar mollis. Vestibulum sem magna, elementum ut, vestibulum eu, facilisis quis, arcu. Mauris a dolor. Nulla facilisi. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Sed blandit. Phasellus pellentesque, ante nec iaculis dapibus, eros justo auctor lectus, a lobortis lorem mauris quis nunc. Praesent pellentesque facilisis elit. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos hymenaeos. In hac habitasse platea dictumst. Mauris a dolor. Nulla facilisi. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.</p>
+					<p><a href="#" class="button1">Learn More</a></p>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+<div id="footer">
+	<p></p>
+</div>
+</body>
+</html>
